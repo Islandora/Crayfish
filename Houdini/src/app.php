ab849f9834eac664a31082ecedf4eb57fc5a8ddc
Jonathan Green <jonathan@razzard.com>
2017-04-12 17:34:15 -0300

    Update Houdini with HWT security

--- Houdini/src/app.php
+++ Houdini/src/app.php
@@ -5,44 +5,89 @@ require_once __DIR__.'/../vendor/autoload.php';
 use Islandora\Chullo\FedoraApi;
 use Islandora\Crayfish\Commons\FedoraResourceConverter;
 use Islandora\Crayfish\Commons\CmdExecuteService;
+use Islandora\Crayfish\Commons\Syn\SettingsParser;
+use Islandora\Crayfish\Commons\Syn\JwtAuthenticator;
+use Islandora\Crayfish\Commons\Syn\JwtFactory;
 use Islandora\Houdini\Controller\HoudiniController;
 use Silex\Application;
 use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\SecurityServiceProvider;
 use Silex\Provider\MonologServiceProvider;
+use Monolog\Logger;
+use Monolog\Handler\NullHandler;
 
 $config = require_once(__DIR__ . '/../cfg/cfg.php');
 
 $app = new Application();
 
-$app->register(new MonologServiceProvider(), [
-    'monolog.logfile' => $config['logfile'],
-    'monolog.level' => $config['loglevel'],
-    'monolog.name' => 'Houdini',
-]);
+// Only use logger if configured
+if (strtolower($config['loglevel']) === 'none') {
+    $app['monolog'] = function () {
+        return new Logger('null', [new NullHandler()]);
+    };
+} else {
+    $app->register(new MonologServiceProvider(), [
+        'monolog.logfile' => $config['logfile'],
+        'monolog.level' => $config['loglevel'],
+        'monolog.name' => 'Houdini',
+    ]);
+}
 
 $app->register(new ServiceControllerServiceProvider());
 
-$app['houdini.controller'] = function () use ($config, $app) {
+$app['houdini.controller'] = function ($app) use ($config) {
     return new HoudiniController(
-        new CmdExecuteService($app['monolog']),
+        new CmdExecuteService($app['monolog']->withName('CmdExecuteService')),
         $config['valid formats'],
         $config['default format'],
         $config['executable'],
         $app['monolog']
     );
 };
+
 $app['fedora_resource.converter'] = function () use ($config) {
     return new FedoraResourceConverter(
         FedoraApi::create($config['fedora base url'])
     );
 };
 
+$app['syn.settings_parser'] = function ($app) use ($config) {
+    $xml = file_get_contents($config['users config']);
+    return new SettingsParser(
+        $xml,
+        $app['monolog']->withName('syn.settings_parser')
+    );
+};
+
+$app['syn.jwt_authentication'] = function ($app) {
+    return new JwtAuthenticator(
+        $app['syn.settings_parser'],
+        new JwtFactory(),
+        $app['monolog']->withName('syn.jwt_authentication')
+    );
+};
+
+if ($config['security enabled']) {
+    $app->register(new SecurityServiceProvider());
+    $app['security.firewalls'] = [
+      'default' => [
+        'stateless' => true,
+        'anonymous' => false,
+        'guard' => [
+          'authenticators' => [
+            'syn.jwt_authentication'
+          ],
+        ],
+      ],
+    ];
+}
+
 $app->get('/convert/{fedora_resource}', "houdini.controller:convert")
     ->assert('fedora_resource', '.+')
     ->convert('fedora_resource', 'fedora_resource.converter:convert');
 
 $app->get('/identify/{fedora_resource}', "houdini.controller:identify")
-  ->assert('fedora_resource', '.+')
-  ->convert('fedora_resource', 'fedora_resource.converter:convert');
+    ->assert('fedora_resource', '.+')
+    ->convert('fedora_resource', 'fedora_resource.converter:convert');
 
 return $app;
