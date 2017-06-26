e5c790a3ebc486d82f827558f72d20a7dd35e48d
dannylamb <dlamb@islandora.ca>
2017-06-26 18:13:49 +0000

    Changing up Gemini to use newer simpler UrlMapper with UUIDs

--- Gemini/src/Controller/GeminiController.php
+++ Gemini/src/Controller/GeminiController.php
@@ -2,8 +2,9 @@
 
 namespace Islandora\Gemini\Controller;
 
-use Islandora\Crayfish\Commons\IdMapper\IdMapperInterface;
+use Islandora\Crayfish\Commons\UrlMapper\UrlMapperInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -14,138 +15,80 @@ class GeminiController
 {
 
     /**
-     * @var \Islandora\Crayfish\Commons\IdMapper\IdMapperInterface
+     * @var \Islandora\Crayfish\Commons\UrlMapper\UrlMapperInterface
      */
-    protected $idMapper;
+    protected $urlMapper;
 
     /**
      * GeminiController constructor.
-     * @param \Islandora\Crayfish\Commons\IdMapper\IdMapperInterface
+     * @param \Islandora\Crayfish\Commons\UrlMapper\UrlMapperInterface
      */
-    public function __construct(IdMapperInterface $idMapper)
+    public function __construct(UrlMapperInterface $urlMapper)
     {
-        $this->idMapper = $idMapper;
+        $this->urlMapper = $urlMapper;
     }
 
     /**
-     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param string $uuid
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function getMetadataId(Request $request)
+    public function get($uuid)
     {
-        $drupal = $request->query->get('drupal', null);
-        if (!$drupal) {
-            return new Response("Missing 'drupal' query param", 400);
-        }
-
         try {
-            if (!$result = $this->idMapper->getMetadataId($drupal)) {
+            $result = $this->urlMapper->getUrls($uuid);
+            if (empty($result)) {
                 return new Response(null, 404);
             }
 
-            return new Response($result, 200);
+            return new JsonResponse($result, 200);
         } catch (\Exception $e) {
             return new Response($e->getMessage(), 500);
         }
     }
 
     /**
+     * @param string $uuid
      * @param \Symfony\Component\HttpFoundation\Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function getBinaryId(Request $request)
+    public function put($uuid, Request $request)
     {
-        $drupal = $request->query->get('drupal', null);
-        if (!$drupal) {
-            return new Response("Missing 'drupal' query param", 400);
+        // Reject non json requests.
+        if (0 !== strpos($request->headers->get('Content-Type'), 'application/json')) {
+            return new Response("Invalid Content-Type.  Expecting application/json", 400);
+        }
+
+        $urls = json_decode($request->getContent(), true);
+
+        // Parse request reject malformed bodies.
+        if (!isset($urls['drupal_rdf'])) {
+            return new Response("Missing 'drupal_rdf' entry in reqeust body.", 400);
+        }
+        $drupal_rdf = $urls['drupal_rdf'];
+
+        if (!isset($urls['fedora_rdf'])) {
+            return new Response("Missing 'fedora_rdf' entry in reqeust body.", 400);
+        }
+        $fedora_rdf = $urls['fedora_rdf'];
+
+        $drupal_nonrdf = isset($urls['drupal_nonrdf']) ? $urls['drupal_nonrdf'] : null;
+        if (count($urls) > 2 && empty($drupal_nonrdf)) {
+            return new Response("Missing 'drupal_nonrdf' entry in reqeust body.", 400);
+        }
+
+        $fedora_nonrdf = isset($urls['fedora_nonrdf']) ? $urls['fedora_nonrdf'] : null;
+        if (count($urls) > 2 && empty($fedora_nonrdf)) {
+            return new Response("Missing 'fedora_nonrdf' entry in reqeust body.", 400);
         }
 
         try {
-            if (!$result = $this->idMapper->getBinaryId($drupal)) {
-                return new Response(null, 404);
-            }
-
-            return new Response($result, 200);
-        } catch (\Exception $e) {
-            return new Response($e->getMessage(), 500);
-        }
-    }
-
-    /**
-     * @param \Symfony\Component\HttpFoundation\Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function saveMetadataId(Request $request)
-    {
-        $drupal = $request->query->get('drupal', null);
-        if (!$drupal) {
-            return new Response("Missing 'drupal' query param", 400);
-        }
-
-        $fedora = $request->query->get('fedora', null);
-        if (!$fedora) {
-            return new Response("Missing 'fedora' query param", 400);
-        }
-
-        try {
-            $this->idMapper->saveMetadataId(
-                $drupal,
-                $fedora
+            $this->urlMapper->saveUrls(
+                $uuid,
+                $drupal_rdf,
+                $fedora_rdf,
+                $drupal_nonrdf,
+                $fedora_nonrdf
             );
-             return new Response(null, 201);
-        } catch (\Exception $e) {
-            return new Response($e->getMessage(), 500);
-        }
-    }
-
-    /**
-     * @param \Symfony\Component\HttpFoundation\Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function saveBinaryId(Request $request)
-    {
-        $drupal = $request->query->get('drupal', null);
-        if (!$drupal) {
-            return new Response("Missing 'drupal' query param", 400);
-        }
-
-        $fedora = $request->query->get('fedora', null);
-        if (!$fedora) {
-            return new Response("Missing 'fedora' query param", 400);
-        }
-
-        $describedby = $request->query->get('describedby', null);
-        if (!$describedby) {
-            return new Response("Missing 'describedby' query param", 400);
-        }
-
-        try {
-            $this->idMapper->saveBinaryId(
-                $drupal,
-                $fedora,
-                $describedby
-            );
-            return new Response(null, 201);
-        } catch (\Exception $e) {
-            return new Response($e->getMessage(), 500);
-        }
-    }
-    /**
-     * @param \Symfony\Component\HttpFoundation\Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function deleteMetadataId(Request $request)
-    {
-        $drupal = $request->query->get('drupal', null);
-        if (!$drupal) {
-            return new Response("Missing 'drupal' query param", 400);
-        }
-
-        try {
-            if (!$result = $this->idMapper->deleteMetadataId($drupal)) {
-                return new Response("Not Found", 404);
-            }
-
             return new Response(null, 204);
         } catch (\Exception $e) {
             return new Response($e->getMessage(), 500);
@@ -153,24 +96,17 @@ class GeminiController
     }
 
     /**
-     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param string $uuid
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function deleteBinaryId(Request $request)
+    public function delete($uuid)
     {
-        $describedby = $request->query->get('describedby', null);
-        if (!$describedby) {
-            return new Response("Missing 'describedby' query param", 400);
-        }
-
         try {
-            if (!$result = $this->idMapper->deleteBinaryId($describedby)) {
-                return new Response("Not Found", 404);
-            }
-
+            $this->urlMapper->deleteUrls($uuid);
             return new Response(null, 204);
         } catch (\Exception $e) {
             return new Response($e->getMessage(), 500);
         }
     }
+
 }
--- Gemini/src/app.php
+++ Gemini/src/app.php
@@ -3,7 +3,7 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Islandora\Crayfish\Commons\Provider\IslandoraServiceProvider;
-use Islandora\Crayfish\Commons\IdMapper\IdMapper;
+use Islandora\Crayfish\Commons\UrlMapper\UrlMapper;
 use Islandora\Crayfish\Commons\Provider\YamlConfigServiceProvider;
 use Islandora\Gemini\Controller\GeminiController;
 use Silex\Application;
@@ -15,17 +15,14 @@ $app->register(new YamlConfigServiceProvider(__DIR__ . '/../cfg/config.yaml'));
 
 $app['gemini.controller'] = function ($app) {
     return new GeminiController(
-        new IdMapper($app['db'])
+        new UrlMapper($app['db'])
     );
 };
 
-$app->get('/metadata', "gemini.controller:getMetadataId");
-$app->get('/binary', "gemini.controller:getBinaryId");
+$app->get('/{uuid}', "gemini.controller:get");
 
-$app->put('/metadata', "gemini.controller:saveMetadataId");
-$app->put('/binary', "gemini.controller:saveBinaryId");
+$app->put('/{uuid}', "gemini.controller:put");
 
-$app->delete('/metadata', "gemini.controller:deleteMetadataId");
-$app->delete('/binary', "gemini.controller:deleteBinaryId");
+$app->delete('/{uuid}', "gemini.controller:delete");
 
 return $app;
