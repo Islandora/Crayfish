bbc245b614953e3db4f3f8fedc216368c70bb1b4
Diego Pino Navarro <dpino@metro.org>
2018-05-30 12:27:31 -0400

    Schema deployment and removal via console
    using Doctrines Migration

---
+++ Gemini/src/Migrations/Version20180530031926.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Islandora\Gemini\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20180530031926 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
+
+        $this->addSql(
+            'CREATE TABLE Gemini (fedora_hash VARCHAR(128) NOT NULL, 
+            drupal_hash VARCHAR(128) NOT NULL, uuid VARCHAR(36) NOT NULL, 
+            drupal_uri LONGTEXT NOT NULL, fedora_uri LONGTEXT NOT NULL, 
+            date_created DATETIME NOT NULL, date_updated DATETIME NOT NULL, 
+            UNIQUE KEY(fedora_hash, drupal_hash), PRIMARY KEY(uuid)) 
+            DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB'
+        );
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
+        $this->addSql('DROP TABLE Gemini');
+    }
+}
